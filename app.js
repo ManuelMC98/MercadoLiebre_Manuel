29d06d3b535166ca0d4243eb1b1c8ad206e3a407
Manuel Muract Correa <manuelmuract98@gmail.com>
2023-10-18 12:05:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/register.html'));
+});
+
+app.post('/register', (req, res) => {
+    res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/login.html'));
+});
+
+app.post('/login', (req, res) => {
+    res.redirect('/');
+});
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => {
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+});
